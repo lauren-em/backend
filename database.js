fa523af6312a249d8e707a0c9498bb06181c90f5
Lauren Byrne <lbyrne7@my.bcit.ca>
2023-01-24 14:06:11 -0800

    updated env imports to match railway naming

--- database.js
+++ database.js
@@ -4,11 +4,11 @@ dotenv.config()
 
 const pool = mysql
   .createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT || 3306,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT || 3306,
   })
   .promise()
 
