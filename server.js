9df9ef89b332e2568bce6838f683199cdf696e9c
Lauren Byrne <lbyrne7@my.bcit.ca>
2023-01-24 13:57:23 -0800

    moved image storage to s3 bucket

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 import express from 'express'
-import fs from 'fs'
 import multer from 'multer'
 import DatabaseOps from './database.js'
+import crypto from 'crypto'
+import * as s3 from './s3.js'
+import sharp from 'sharp'
 
 const app = express()
-const upload = multer({ dest: 'images/' })
+
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+
+const generateFileName = (bytes = 32) =>
+  crypto.randomBytes(bytes).toString('hex')
+
 const db = new DatabaseOps()
 
 app.use(express.json())
@@ -20,25 +28,34 @@ app.get('/api/images', async (req, res) => {
   let filter = req.query.filter
   console.log(filter)
   const images = await db.getImages(filter)
-  console.log(images)
+
+  for (const image of images) {
+    image.imageURL = await s3.getSignedUrl(image.fileName)
+  }
+
   res.send(images)
 })
 
 app.post('/api/images', upload.single('image'), async (req, res) => {
-  console.log(req.file.filename)
-  const image = await db.addImage(
-    req.file.filename,
-    req.body.description,
-    req.body.type
-  )
-  console.log(image)
-  res.send(image)
-})
+  // Get the data from the post request
+  const description = req.body.description
+  const imageType = req.body.type
+  const mimetype = req.file.mimetype
+  const fileName = generateFileName()
 
-app.get('/api/images/:fileName', (req, res) => {
-  const fileName = req.params.fileName
-  const readStream = fs.createReadStream(`images/${fileName}`)
-  readStream.pipe(res)
+  // process image
+  const fileBuffer = await sharp(req.file.buffer).grayscale().toBuffer()
+
+  // Store the image in s3
+  const s3Result = await s3.uploadImage(fileBuffer, fileName, mimetype)
+
+  // Store the image in the database
+  const databaseResult = await db.addImage(fileName, description, imageType)
+
+  // Get URL to return to send back to front end
+  databaseResult.imageURL = await s3.getSignedUrl(fileName)
+
+  res.status(201).send(databaseResult)
 })
 
 const port = process.env.PORT || 8080
